3b56b09a551c013af2761c1ca9152450c1aac277
Jack20001413 <vivantran2000@gmail.com>
2025-10-05 14:12:32 +0700

    feat(migrations): Add new data migration script

---
+++ CoffeeShopIMS/Migrations/20251005070836_IngredientCreateNewColumnUnit.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeeShopIMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class IngredientCreateNewColumnUnit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Unit",
+                table: "Ingredients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Unit",
+                table: "Ingredients");
+        }
+    }
+}
