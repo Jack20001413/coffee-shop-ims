e012ec81de514f9e924c6b779255bc4c1f6a5ea8
Jack20001413 <vivantran2000@gmail.com>
2025-10-08 21:01:59 +0700

    fix(db): Add seeding data

--- CoffeeShopIMS/Data/ApplicationDbContext.cs
+++ CoffeeShopIMS/Data/ApplicationDbContext.cs
@@ -10,10 +10,38 @@ public class ApplicationDbContext : DbContext
     {
 
     }
-    
+
     public DbSet<Ingredient> Ingredients { get; set; }
     public DbSet<PurchaseOrder> PurchaseOrders { get; set; }
     public DbSet<PurchaseOrderDetail> PurchaseOrderDetails { get; set; }
     public DbSet<Supplier> Suppliers { get; set; }
     public DbSet<Warehouse> Warehouses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        IEnumerable<Ingredient> seedingIngredients =
+        [
+            new() { Name = "Espresso Roast", Unit = "kg", Quantity = 50, Sku = "ER-001"},
+            new() { Name = "Whole Milk", Unit = "liters", Quantity = 100, Sku = "WM-001"},
+            new() { Name = "Sugar", Unit = "kg", Quantity = 200, Sku = "SG-001"},
+            new() { Name = "Vanilla Syrup", Unit = "liters", Quantity = 50, Sku = "VS-001"}
+        ];
+        
+        optionsBuilder.UseSeeding((context, _) =>
+        {
+            if (Ingredients.Any() is false)
+            {
+                Ingredients.AddRange(seedingIngredients);
+                context.SaveChanges();
+            }
+        })
+        .UseAsyncSeeding(async (context, _, cancellationToken) =>
+        {
+            if (await Ingredients.AnyAsync(cancellationToken) is false)
+            {
+                await Ingredients.AddRangeAsync(seedingIngredients, cancellationToken);
+                await context.SaveChangesAsync(cancellationToken);
+            }
+        });
+    }
 }
